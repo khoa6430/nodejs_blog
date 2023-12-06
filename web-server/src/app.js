b7882624340fe6010664236f2bdf3bcfb857b3d8
Lê Đăng Khoa <khoa6430@gmail.com>
2023-12-06 16:35:54 +0700

    update part 7

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,39 +1,78 @@
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const app = express();
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-console.log(__dirname)
-console.log(path.join(__dirname,'../'))
+// Setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
-const app = express()
-const publicDirectoryPath = path.join(__dirname,'../public');
-// console log  D:\work\nodejs_blog\web-server\public
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Khoa Le",
+  });
+});
+// app.get("", (req, res) => {
+//   res.send("<h1>Hello express!</h1>");
+// });
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Khoa Le",
+  });
+});
 
-app.use(express.static(publicDirectoryPath))
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Khoa Le",
+  });
+});
 
-app.get('',(req,res)=>{
-    res.send('<h1>Hello express!</h1>')
-})
-app.get('/help',(req,res)=>{
-    res.send([{
-        name:'Khoa',
-        age:24
-    },
-    {
-        name:'Khoa 2',
-        age:24
-    },
-])
-})
-app.get('/weather', (req, res) => {
-    res.send({
-    forecast: 'It is snowing',
-    location: 'Philadelphia'
-    })
-    })
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+
+// app.get("/help", (req, res) => {
+//   res.send([
+//     {
+//       name: "Khoa",
+//       age: 24,
+//     },
+//     {
+//       name: "Khoa 2",
+//       age: 24,
+//     },
+//   ]);
+// });
+app.get("/weather", (req, res) => {
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Khoa Le",
+    errorMessage: "Page not found.",
+  });
+});
 // app.com
 // app.com/help
 // app.com/about
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000.')
-}) 
- 
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
