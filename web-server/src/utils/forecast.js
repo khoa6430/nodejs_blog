2857cd78686a22a711cc65e3512b2983b8179330
Lê Đăng Khoa <khoa6430@gmail.com>
2023-12-10 15:28:39 +0700

    add part 9 lesson

--- web-server/src/utils/forecast.js
+++ web-server/src/utils/forecast.js
@@ -4,6 +4,7 @@ const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=df45ec35d0d2ba710a77a306964f7702&query=${latitude},${longitude}$units=f`;
 
   request({ url, json: true }, (error, { body }) => {
+    console.log("body:", body);
     if (error) {
       callback("Unable to connect to weather service!", undefined);
     } else if (body.error) {
@@ -11,11 +12,14 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        // body.daily.data[0]?.summary +
-        " It is currently " + body.current?.temperature + " degress out."
-        //   body.currently?.precipProbability +
-        //   "% chance of rain."
-        // body.current
+        body.current.weather_descriptions[0] +
+          " It is currently " +
+          body.current.temperature +
+          " degress out. This high today is " +
+          body.current.feelslike +
+          " degress out.The humidity is " +
+          body.current.humidity +
+          " %"
       );
     }
   });
