98c2d13beca1c81fd3eba94f31f9e76e9c481ad5
Lê Đăng Khoa <khoa6430@gmail.com>
2023-12-11 22:11:38 +0700

    update lesson 10

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,90 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api");
+
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Email is invalid");
+//       }
+//     },
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 7,
+//     trim: true,
+//     validate(value) {
+//       if (value.toLowerCase().includes("password")) {
+//         throw new Error('Password cannot contain "password"');
+//       }
+//     },
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("Age must be a positive number");
+//       }
+//     },
+//   },
+// });
+
+// async function run() {
+//   const me = new User({
+//     name: " Khoa    ",
+//     email: "KHOA6030@GMAIL.COM",
+//     password: "phone098!",
+//   });
+
+//   await me
+//     .save()
+//     .then(() => {
+//       console.log(me);
+//     })
+//     .catch((error) => {
+//       console.log("Error!", error);
+//     });
+// }
+
+// run();
+
+// const Task = mongoose.model("Task", {
+//   description: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   completed: {
+//     type: Boolean,
+//     default: false,
+//   },
+// });
+
+// async function runTask() {
+//   const task = new Task({
+//     description: "  Eat lunch",
+//   });
+
+//   await task
+//     .save()
+//     .then(() => {
+//       console.log(task);
+//     })
+//     .catch((error) => {
+//       console.log("Error!", error);
+//     });
+// }
+
+// runTask();
