0c990eebeab36cea8c8c6095cfa87c7498be1a0c
Lê Đăng Khoa <khoa6430@gmail.com>
2023-12-16 18:14:07 +0700

    update lesson 16

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,60 +1,7 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-const jwt = require("jsonwebtoken");
+const app = require("./app");
 const { PORT } = require("../config/endpoints");
 
-const app = express();
 const port = PORT;
-
-const multer = require("multer");
-
-// const upload = multer({
-//   dest: "images",
-//   fileFilter(req, file, cb) {
-//     if (!file.originalname.match(/\.(doc|docx)$/)) {
-//       return cb(new Error("Please upload a Word document"));
-//     }
-
-//     cb(undefined, true);
-//   },
-// });
-
-// app.post("/upload", upload.single("upload"), (req, res) => {
-//   res.send();
-// });
-
-const upload = multer({
-  dest: "images",
-  limits: {
-    fileSize: 1000000,
-  },
-  fileFilter(req, file, cb) {
-    console.log("file:", file.originalname);
-    if (!file.originalname.match(/\.(doc|docx)$/)) {
-      return cb(new Error("Please upload a Word document"));
-    }
-
-    cb(undefined, true);
-  },
-});
-
-app.post(
-  "/upload",
-  upload.single("upload"),
-  (req, res) => {
-    res.send();
-  },
-  (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-  }
-);
-
-app.use(express.json());
-
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
