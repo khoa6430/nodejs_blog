45d7e1bdee68724b5b4f133f540cdd938a2d9ea2
Lê Đăng Khoa <khoa6430@gmail.com>
2023-12-17 16:59:39 +0700

    update style chat app

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,6 +3,10 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -14,35 +18,35 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
 io.on("connection", (socket) => {
-  io.on("connection", (socket) => {
-    console.log("New WebSocket connection");
+  console.log("New WebSocket connection");
 
-    socket.emit("message", "Welcome!");
+  socket.emit("message", generateMessage("Welcome!"));
 
-    socket.broadcast.emit("message", "A new user has joined!");
+  socket.broadcast.emit("message", "A new user has joined!");
 
-    socket.on("sendMessage", (message, callback) => {
-      const filter = new Filter();
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
 
-      if (filter.isProfane(message)) {
-        return callback("Profanity is not allowed!");
-      }
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed!");
+    }
 
-      io.emit("message", message);
-      callback();
-    });
+    io.emit("message", generateMessage(message));
+    callback();
+  });
 
-    socket.on("sendLocation", (coords, callback) => {
-      io.emit(
-        "locationMessage",
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "locationMessage",
+      generateLocationMessage(
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
-      );
-      callback();
-    });
+      )
+    );
+    callback();
+  });
 
-    socket.on("disconnect", () => {
-      io.emit("message", "A user has left!");
-    });
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
   });
 });
 
